20c8e56e73b8188a7942a5a838b5dc719c9dea31
Roman Kolyagin <profesorhiro@gmail.com>
2023-01-23 00:06:31 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -183,10 +183,10 @@ public class StartUITest {
     public void whenInvalidExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] { "10", "0"}
+                new String[]{"10", "0"}
         );
         Tracker tracker = new Tracker();
-        UserAction[] actions = new UserAction[] {
+        UserAction[] actions = new UserAction[]{
                 new ExitAction(out)
         };
         new StartUI(out).init(in, tracker, actions);
