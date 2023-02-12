94cb78fbe601556120903bd7191d414e34b69ce4
Roman Kolyagin <profesorhiro@gmail.com>
2023-02-12 23:25:49 +0300

    5. Очередь с приоритетом на LinkedList [#41670 #390947]

--- src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -10,7 +10,7 @@ class PriorityQueueTest {
         PriorityQueue queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
-        queue.put(new Task("middle", 3));;
+        queue.put(new Task("middle", 3));
         Task result = queue.take();
         assertThat(result.getDesc()).isEqualTo("urgent");
     }
